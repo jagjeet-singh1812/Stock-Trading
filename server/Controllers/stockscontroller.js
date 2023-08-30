93855bbc2a9c9a8cf2243f59812ca7a8fda11a6c
jagjeet <sappaljagjeet@gmail.com>
2023-08-30 16:45:59 +0530

    updates backend

---
+++ server/Controllers/stockscontroller.js
@@ -0,0 +1,74 @@
+const Alpaca = require("@alpacahq/alpaca-trade-api");
+
+API_KEY = "PKKM0LCA32H89WRZ8A1R";
+API_SECRET = "7QNcVM5RSbi2aWghXVeMTlcnrrGS12fjQxDmBcmG";
+const feed = "iex"; // Change to "sip" if on a paid plan
+const symbol = "BTCUSD";
+
+class DataStream {
+    constructor({ apiKey, secretKey, feed }) {
+      this.alpaca = new Alpaca({
+        keyId: apiKey,
+        secretKey,
+        feed,
+      });
+
+      const socket = this.alpaca.data_stream_v2;
+
+      socket.onConnect(function () {
+        console.log("Connected");
+        socket.subscribeForQuotes(["AAPL"]);
+        socket.subscribeForTrades(["FB"]);
+        socket.subscribeForBars(["SPY"]);
+        socket.subscribeForStatuses(["*"]);
+      });
+
+      socket.onError((err) => {
+        console.log(err);
+      });
+
+      socket.onStockTrade((trade) => {
+        console.log(trade);
+      });
+
+      socket.onStockQuote((quote) => {
+        console.log(quote);
+      });
+
+      socket.onStockBar((bar) => {
+        console.log(bar);
+      });
+
+      socket.onStatuses((s) => {
+        console.log(s);
+      });
+
+      socket.onStateChange((state) => {
+        console.log(state);
+      });
+
+      socket.onDisconnect(() => {
+        console.log("Disconnected");
+      });
+
+      socket.connect();
+
+      // unsubscribe from FB after a second
+      setTimeout(() => {
+        socket.unsubscribeFromTrades(["FB"]);
+      }, 1000);
+    }
+  }
+
+const Realtime_data = async (req, res) => {
+  let stream = new DataStream({
+    apiKey: API_KEY,
+    secretKey: API_SECRET,
+    feed: "sip",
+    paper: true,
+  });
+  console.log(stream);
+  res.status(200).send("success");
+};
+
+module.exports = { Realtime_data };

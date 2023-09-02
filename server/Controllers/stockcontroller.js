c0ac41068dc5a8f745e11d94638b694da49da3f4
Parth Puranik <thosparth@gmail.com>
2023-09-02 15:01:29 +0530

    update

--- server/Controllers/stockcontroller.js
+++ server/Controllers/stockcontroller.js
@@ -1,74 +1,87 @@
 const Alpaca = require("@alpacahq/alpaca-trade-api");
+const CircularJSON = require("circular-json");
 
 API_KEY = "PKKM0LCA32H89WRZ8A1R";
 API_SECRET = "7QNcVM5RSbi2aWghXVeMTlcnrrGS12fjQxDmBcmG";
 const feed = "iex"; // Change to "sip" if on a paid plan
 const symbol = "BTCUSD";
-
+// let companySymbol,price;
+let quote_data = {};
 class DataStream {
-    constructor({ apiKey, secretKey, feed }) {
-      this.alpaca = new Alpaca({
-        keyId: apiKey,
-        secretKey,
-        feed,
-      });
+  constructor({ apiKey, secretKey, feed }) {
+    this.alpaca = new Alpaca({
+      keyId: apiKey,
+      secretKey,
+      feed,
+    });
 
-      const socket = this.alpaca.data_stream_v2;
+    const socket = this.alpaca.data_stream_v2;
 
-      socket.onConnect(function () {
-        console.log("Connected");
-        socket.subscribeForQuotes(["AAPL"]);
-        socket.subscribeForTrades(["FB"]);
-        socket.subscribeForBars(["SPY"]);
-        socket.subscribeForStatuses(["*"]);
-      });
+    socket.onConnect(function () {
+      console.log("Connected");
+      socket.subscribeForQuotes(["AAPL", "GOOG", "MSFT"]);
+      // socket.subscribeForTrades(["FB"]);
+      // socket.subscribeForBars(["SPY"]);
+      // socket.subscribeForStatuses(["*"]);
+    });
 
-      socket.onError((err) => {
-        console.log(err);
-      });
+    socket.onError((err) => {
+      console.log(err);
+    });
 
-      socket.onStockTrade((trade) => {
-        console.log(trade);
-      });
+    // socket.onStockTrade((trade) => {
+    //   console.log(trade);
+    // });
 
-      socket.onStockQuote((quote) => {
-        console.log(quote);
-      });
+    socket.onStockQuote((quote) => {
+      console.log(quote);
+      quote_data = quote;
+    });
 
-      socket.onStockBar((bar) => {
-        console.log(bar);
-      });
+    // socket.onStockBar((bar) => {
+    //   console.log(bar);
+    // });
 
-      socket.onStatuses((s) => {
-        console.log(s);
-      });
+    socket.onStatuses((s) => {
+      console.log(s);
+    });
 
-      socket.onStateChange((state) => {
-        console.log(state);
-      });
+    socket.onStateChange((state) => {
+      console.log(state);
+    });
 
-      socket.onDisconnect(() => {
-        console.log("Disconnected");
-      });
+    socket.onDisconnect(() => {
+      console.log("Disconnected");
+    });
 
-      socket.connect();
+    socket.connect();
 
-      // unsubscribe from FB after a second
-      setTimeout(() => {
-        socket.unsubscribeFromTrades(["FB"]);
-      }, 1000);
-    }
+    // unsubscribe from FB after a second
+    setTimeout(() => {
+      socket.unsubscribeFromTrades(["FB"]);
+    }, 1000);
   }
+}
 
 const Realtime_data = async (req, res) => {
-  let stream = new DataStream({
-    apiKey: API_KEY,
-    secretKey: API_SECRET,
-    feed: "sip",
-    paper: true,
-  });
+
+    let stream = new DataStream({
+      apiKey: API_KEY,
+      secretKey: API_SECRET,
+      feed: "sip",
+      paper: true,
+    })
+    // if(stream){
+    // res.status(200).json(quote_data);
+    // }
+    let resp = res.status(200).json(quote_data)
+  
+  // const circularStream = CircularJSON.stringify(stream);
+  // const parsedStream = CircularJSON.parse(circularStream);
   console.log(stream);
-  res.status(200).send("success");
+  // let data = {
+  //   quote_data,
+  // };
 };
 
 module.exports = { Realtime_data };
